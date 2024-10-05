141f2db16353a8376fbde91a93f98e78f166767d
Jojo <neeya.blog@gmail.com>
2024-10-05 09:25:35 +0800

    version update, replace cache?

--- service-worker.js
+++ service-worker.js
@@ -24,7 +24,7 @@ const urlsToCache = [
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    fetch(VERSION_FILE)
+    fetch(VERSION_FILE, { cache: 'no-cache' })
       .then(response => response.json())
       .then(version => {
         CURRENT_CACHE_NAME = BASE_CACHE_NAME + version.version;
@@ -33,19 +33,21 @@ self.addEventListener('install', (event) => {
       .then(cache => {
         return cache.addAll(urlsToCache.map(url => new Request(url, {credentials: 'same-origin'})));
       })
+      .then(() => self.skipWaiting()) // Activate the new service worker immediately
   );
 });
 
 self.addEventListener('activate', (event) => {
   event.waitUntil(
     caches.keys().then(cacheNames => {
-      return Promise.all(
+      return Promise.all([
         cacheNames.map(cacheName => {
           if (cacheName.startsWith(BASE_CACHE_NAME) && cacheName !== CURRENT_CACHE_NAME) {
             return caches.delete(cacheName);
           }
-        })
-      );
+        }),
+        self.clients.claim() // Take control of all open clients
+      ]);
     })
   );
 });
@@ -58,25 +60,18 @@ self.addEventListener('fetch', (event) => {
   }
 
   event.respondWith(
-    caches.match(event.request)
-      .then((response) => {
-        if (response) {
-          return response;
+    fetch(event.request)
+      .then(networkResponse => {
+        if (networkResponse.ok) {
+          const clonedResponse = networkResponse.clone();
+          caches.open(CURRENT_CACHE_NAME).then(cache => {
+            cache.put(event.request, clonedResponse);
+          });
+          return networkResponse;
         }
-        return fetch(event.request).then(
-          (response) => {
-            if(!response || response.status !== 200 || response.type !== 'basic') {
-              return response;
-            }
-            const responseToCache = response.clone();
-            caches.open(CURRENT_CACHE_NAME)
-              .then((cache) => {
-                cache.put(event.request, responseToCache);
-              });
-            return response;
-          }
-        );
+        throw new Error('Network response was not ok.');
       })
+      .catch(() => caches.match(event.request))
   );
 });
 
