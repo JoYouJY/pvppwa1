67b9a2ae92dafe37c8bb88dcb55dd4e3beb7d7f0
Jojo <neeya.blog@gmail.com>
2024-10-06 08:26:44 +0800

    added PWA auto check version and clean Cache then force shift F5

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,7 @@
 const CACHE_NAME = 'fa-rpg-cache-v1';
 const urlsToCache = [
   '/',
+  '/version.json',
   '/index.html',
   '/TemplateData/style.css',
   '/index.js',
@@ -19,35 +20,53 @@ const urlsToCache = [
   '/TemplateData/progress-bar-full-dark.png'
 ];
 
+// During the installation, cache the assets
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then((cache) => {
-        return cache.addAll(urlsToCache.map(url => new Request(url, {credentials: 'same-origin'})));
-      })
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache.map(url => new Request(url, { credentials: 'same-origin' })));
+    })
   );
 });
 
+
+// Activate and clean up old caches
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = [CACHE_NAME]; // Only keep the current version
+
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            // Delete old caches
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Fetch handler to serve cached content and update cache if needed
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request)
-      .then((response) => {
-        if (response) {
+    caches.match(event.request).then((response) => {
+      if (response) {
+        return response; // Return from cache
+      }
+      return fetch(event.request).then((response) => {
+        if (!response || response.status !== 200 || response.type !== 'basic') {
           return response;
         }
-        return fetch(event.request).then(
-          (response) => {
-            if(!response || response.status !== 200 || response.type !== 'basic') {
-              return response;
-            }
-            const responseToCache = response.clone();
-            caches.open(CACHE_NAME)
-              .then((cache) => {
-                cache.put(event.request, responseToCache);
-              });
-            return response;
-          }
-        );
-      })
+
+        const responseToCache = response.clone();
+        caches.open(CACHE_NAME).then((cache) => {
+          cache.put(event.request, responseToCache); // Update cache with new response
+        });
+
+        return response;
+      });
+    })
   );
 });
